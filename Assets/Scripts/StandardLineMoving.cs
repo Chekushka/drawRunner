384dc67d20856112f5fcbf376b854e840636de81
Chekushka <chekun.sergiy@gmail.com>
2022-03-13 14:35:41 +0200

    Fix for drawing after jetpack

--- Assets/Scripts/StandardLineMoving.cs
+++ Assets/Scripts/StandardLineMoving.cs
@@ -6,7 +6,6 @@ public class StandardLineMoving : MonoBehaviour
 {
     [SerializeField] private Transform drawObjectTransform;
     private CharacterMovement _characterMovement;
-    private bool _isMovingUpDone;
 
     private void Start()
     {
@@ -23,17 +22,14 @@ public class StandardLineMoving : MonoBehaviour
         var linePoints = new Vector3[line.positionCount];
         line.GetPositions(linePoints);
 
-        if (!_isMovingUpDone && _characterMovement.isOnHighPlatform)
+        for (var i = 0; i < linePoints.Length; i++)
         {
-            for (var i = 0; i < linePoints.Length; i++)
+            linePoints[i].z = drawObjectTransform.position.z;
+            if(_characterMovement.isOnHighPlatform)
                 linePoints[i].y += _characterMovement.GetHighPlatformHeight();
-            _isMovingUpDone = true;
         }
 
-        for (var i = 0; i < linePoints.Length; i++)
-            linePoints[i].z = drawObjectTransform.position.z;
-            
-        
+
         line.SetPositions(linePoints);
     }
 }
